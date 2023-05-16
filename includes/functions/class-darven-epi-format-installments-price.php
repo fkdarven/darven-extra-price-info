1bace96e37c6089902aae4781b297341a26438bf
Leticia Moreira <leticia@vivaldi.net>
2023-05-16 09:55:27 -0300

    Refactor: installments class

--- includes/functions/class-darven-epi-format-installments-price.php
+++ includes/functions/class-darven-epi-format-installments-price.php
@@ -57,6 +57,7 @@ if ( ! class_exists( 'Darven_Epi_Format_Installments_Price' ) ) {
             $this->interest_fee_from          = $darven_options['darven_epi_installments_interest_fee_from'] ?: 0;
             $this->interest_fee_first_install = $darven_options['darven_epi_installments_interest_fee_first_install'] ?: 0;
 
+            
 
             $this->mode_of_view            = $darven_options['darven_epi_mode_of_view'];
             $this->installments_popup_text = $darven_options['darven_epi_popup_text'];
